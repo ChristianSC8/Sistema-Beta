bd69ba47143d8e7e44b3415cc75a572c33de5fd0
ChristianSC8 <christian.supo@upeu.edu.pe>
2023-06-22 21:00:05 -0500

    add ui category

---
+++ src/app/main-content/category/category.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent {
+  @ViewChild('open') openBtn!: ElementRef;
+  @ViewChild('container') container!: ElementRef;
+  @ViewChild('close') closeBtn!: ElementRef;
+  @ViewChild('modal') modal!: ElementRef;
+  @ViewChild('cancel') cancel!: ElementRef;
+
+  openModal() {
+    this.container.nativeElement.classList.add('active-filter');
+    this.modal.nativeElement.classList.add('active-modal');
+  }
+
+  closeModal() {
+    this.container.nativeElement.classList.remove('active-filter');
+    this.modal.nativeElement.classList.remove('active-modal');
+    this.cancel.nativeElement.classList.remove('active-modal');
+  }
+
+}
--- src/app/main-content/main-content.module.ts
+++ src/app/main-content/main-content.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StartComponent } from './start/start.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
 
 
 @NgModule({
   declarations: [
-    StartComponent
+    StartComponent,
+    ProductComponent,
+    CategoryComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    StartComponent
+    StartComponent,
+    ProductComponent,
+    CategoryComponent
   ]
 })
 export class MainContentModule { }
